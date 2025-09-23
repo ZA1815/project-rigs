c3817a7b6504ecb8e7581049ab2dcc83d9be5c98
ZA1815 <zaahme18@gmail.com>
2025-09-23 10:49:45 -0400

    feat: Add models for users, comments, and rigs

--- rigs/models.py
+++ rigs/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import mongoengine
 
 # Create your models here.
+
+class User(mongoengine.Document):
+    username = mongoengine.StringField(required=True, unique=True)
+    email = mongoengine.StringField(required=True, unique=True)
+    password = mongoengine.StringField(required=True)
+
+    meta = {'collection': 'users'}
+
+class Comment(mongoengine.Document):
+    text = mongoengine.StringField(required=True, unique=True)
+    author = mongoengine.ReferenceField(User, required=True)
+    created_at = mongoengine.DateTimeField(default=datetime.utcnow)
+
+    meta = {'collection': 'comments'}
+
+class Rig(mongoengine.Document):
+    title = mongoengine.StringField(required=True, max_length=200)
+    image_url = mongoengine.StringField(required=True)
+    description = mongoengine.StringField(required=True, max_length=500)
+    author = mongoengine.ReferenceField(User, required=True)
+    comments = mongoengine.ListField(mongoengine.ReferenceField(Comment))
+    created_at = mongoengine.DateTimeField(default=datetime.utcnow)
+
+    meta = {'collection': 'rigs'}
