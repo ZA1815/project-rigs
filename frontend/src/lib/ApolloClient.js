f83aec34db2986920c3d59cb136cbfafd5c67f23
ZA1815 <zaahme18@gmail.com>
2025-09-24 18:01:01 -0400

    fix: Update apollo client usage syntax

--- frontend/src/lib/ApolloClient.js
+++ frontend/src/lib/ApolloClient.js
@@ -1,7 +1,11 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client';
+
+const httpLink = new HttpLink({
+    uri: 'http://localhost:8000/graphql'
+});
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8000/graphql',
+    link: httpLink,
     cache: new InMemoryCache()
 });
 
