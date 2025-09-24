b160b21dd7d297bf78d5a9fad8bc687a386fed06
ZA1815 <zaahme18@gmail.com>
2025-09-24 16:39:30 -0400

    feat: Add component to display comments list for a rig

---
+++ frontend/src/app/components/CommentsList.js
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function CommentList() {
+    return (
+        <Box>
+            <Typography variant="h6">Comments:</Typography>
+            <Typography>Comments list placeholder</Typography>
+        </Box>
+    );
+}
