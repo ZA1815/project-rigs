fd491d05c9b7135fc55ec13da0e10a17027d86ef
ZA1815 <zaahme18@gmail.com>
2025-09-24 22:04:05 -0400

    feat: add token to headers before querying for security

--- frontend/src/lib/ApolloClient.js
+++ frontend/src/lib/ApolloClient.js
@@ -1,11 +1,27 @@
 import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client';
+import {SetContextLink} from '@apollo/client/link/context'
 
 const httpLink = new HttpLink({
     uri: 'http://localhost:8000/graphql'
 });
 
+const authLink = new SetContextLink((request, previousContext) => {
+    if (typeof window === 'undefined') {
+        return previousContext;
+    }
+
+    const token = localStorage.getItem('authToken');
+
+    return {
+        headers: {
+            ...previousContext.headers,
+            authorization: token ? `JWT ${token}` : ''
+        }
+    };
+})
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: from([authLink, httpLink]),
     cache: new InMemoryCache()
 });
 
