d9fb1db839afe7b1056c5bbc3339aa8775dc4fc5
ZA1815 <zaahme18@gmail.com>
2025-09-24 22:51:54 -0400

    fix: import correct dependency and fix syntax

--- frontend/src/lib/ApolloClient.js
+++ frontend/src/lib/ApolloClient.js
@@ -1,5 +1,5 @@
-import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client';
-import {SetContextLink} from '@apollo/client/link/context'
+import {ApolloClient, InMemoryCache, HttpLink, ApolloLink} from '@apollo/client';
+import {SetContextLink} from '@apollo/client/link/context';
 
 const httpLink = new HttpLink({
     uri: 'http://localhost:8000/graphql'
@@ -21,7 +21,7 @@ const authLink = new SetContextLink((request, previousContext) => {
 })
 
 const client = new ApolloClient({
-    link: from([authLink, httpLink]),
+    link: ApolloLink.from([authLink, httpLink]),
     cache: new InMemoryCache()
 });
 
