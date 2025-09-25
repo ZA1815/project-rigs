ce617ec63cf623b67b75a99375b6725448e95e4a
ZA1815 <zaahme18@gmail.com>
2025-09-25 19:51:42 -0400

    feat: Create commentslist component and display comments underneath rigs

--- frontend/src/app/rigs/[id]/page.js
+++ frontend/src/app/rigs/[id]/page.js
@@ -1,10 +1,12 @@
 'use client';
 
 import RigCard from "@/components/RigCard";
+import CommentsList from "@/components/CommentsList";
 import { gql } from "@apollo/client";
-import { useQuery } from "@apollo/client/react";
-import React from "react";
-import { Box, Typography } from "@mui/material";
+import { useMutation, useQuery } from "@apollo/client/react";
+import React, { useState } from "react";
+import { Box, Typography, TextField, Button } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 const GET_RIG_BY_ID = gql`
     query GetRigById($rigId: ID!) {
@@ -13,6 +15,27 @@ const GET_RIG_BY_ID = gql`
             description
             imageUrl
             createdAt
+            comments {
+                id
+                text
+                author {
+                    username
+                }
+            }
+        }
+    }
+`;
+
+const COMMENT_MUTATION = gql`
+    mutation CreateComment($text: String!, $rigId: ID!) {
+        createComment(text: $text, rigId: $rigId) {
+            comment {
+                id
+                text
+                author {
+                    username
+                }
+            }
         }
     }
 `;
@@ -20,24 +43,80 @@ const GET_RIG_BY_ID = gql`
 export default function RigDetailPage({params}) {
     const unrappedParams = React.use(params);
     const rigId = unrappedParams.id;
+    const [commentText, setCommentText] = useState('');
+    const router = useRouter();
 
-    const {data, loading, error} = useQuery(GET_RIG_BY_ID, {
+    const {data: dataRig, loading: loadingRig, error: errorRig} = useQuery(GET_RIG_BY_ID, {
         variables: {
             rigId: rigId
         }
     });
 
-    if (loading) {
+    const [createComment, {loading: loadingComment, error: errorComment}] = useMutation(COMMENT_MUTATION, {
+        refetchQueries: [
+            {
+                query: GET_RIG_BY_ID,
+                variables: {
+                    rigId
+                }
+            }
+        ],
+        awaitRefetchQueries: true
+    });
+
+    const onChangeFunc = (e) => {
+        setCommentText(e.target.value);
+    }
+
+    const onSubmitFunc = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await createComment({
+                variables: {
+                    text: commentText,
+                    rigId: rigId
+                }
+            });
+            
+            setCommentText('');
+        }
+        catch (err) {
+            if (err.message.includes('Authentication required')) {
+                console.error('Authentication error: Token is invalid or expired.');
+                localStorage.removeItem('authToken');
+                router.push('/login');
+            }
+            else {
+                console.error('An unexpected error occurred:', err);
+            }
+        }
+    }
+
+
+    if (loadingRig) {
         return <Typography>Loading rig...</Typography>
     }
 
-    if (error) {
+    if (errorRig) {
         return <Typography>Error loading rig: {error.message}</Typography>
+    }
+
+    if (!dataRig?.rigById) {
+        return <Typography>Rig not found.</Typography>
     }
 
     return (
         <Box sx={{p: 4}}>
-            <RigCard key={rigId} rig={data.rigById}/>
+            <RigCard key={rigId} rig={dataRig.rigById}/>
+            <CommentsList comments={dataRig.rigById.comments}/>
+            <Box component="form" onSubmit={onSubmitFunc} sx={{display: 'flex', flexDirection: 'column', gap: 2, width: '300px', margin: 'auto'}}>
+                <Typography variant="h3">Add a comment:</Typography>
+                <TextField label="Text:" value={commentText} onChange={onChangeFunc} required/>
+                {loadingComment ? <Button type="submit" variant="contained" disabled>Submitting...</Button> : <Button type="submit" variant="contained">Submit</Button>}
+                {errorComment && (
+                    <Typography color="error">Error: {errorComment.message}</Typography>
+                )}
+            </Box>
         </Box>
     );
 }
--- frontend/src/components/CommentsList.js
+++ frontend/src/components/CommentsList.js
@@ -1,11 +1,21 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import {List, ListItem, ListItemText, Divider, Typography, dividerClasses} from '@mui/material';
+import React from 'react';
 
-export default function CommentList() {
+export default function CommentList({comments}) {
+    if (!comments || comments.length === 0) {
+        return <Typography>No comments yet.</Typography>
+    }
+
     return (
-        <Box>
-            <Typography variant="h6">Comments:</Typography>
-            <Typography>Comments list placeholder</Typography>
-        </Box>
+        <List>
+            {comments.map((comment, index) => (
+                <div key={comment.id}>
+                    <ListItem >
+                        <ListItemText primary={comment.text} secondary={`--- ${comment.author.username}`}/>
+                    </ListItem>
+                    {index < comments.length - 1 && <Divider component="li" />}
+                </div>
+            ))}
+        </List>
     );
 }
