3d2d61e98c651714b174ecccec2ec2368ef36a9a
ZA1815 <zaahme18@gmail.com>
2025-09-26 10:44:54 -0400

    style: Add new font to apply to whole project

--- frontend/src/ThemeRegistry.js
+++ frontend/src/ThemeRegistry.js
@@ -3,10 +3,20 @@ import * as react from 'react';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {AppRouterCacheProvider} from '@mui/material-nextjs/v13-appRouter';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['300', '600'],
+    display: 'swap'
+})
 
 const theme = createTheme({
     palette: {
         mode: 'dark'
+    },
+    typography: {
+        fontFamily: poppins.style.fontFamily
     }
 })
 
