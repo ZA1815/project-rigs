1659d10809c43c1fdd71fed2951924f6518357b7
ZA1815 <zaahme18@gmail.com>
2025-09-26 10:43:26 -0400

    style: resize card and adjust font weight

--- frontend/src/components/RigCard.js
+++ frontend/src/components/RigCard.js
@@ -2,11 +2,11 @@ import {CardMedia, Card, CardContent, Typography} from '@mui/material';
 
 export default function RigCard({rig}) {
     return (
-        <Card>
-            <CardContent>
-                <Typography variant="h5">{rig.title}</Typography>
-                <Typography variant="body2">{rig.description}</Typography>
-                <CardMedia component="img" height="194" image={rig.imageUrl} alt={rig.title}/>
+        <Card sx={{height: '100%', minWidth: 500, minHeight: 380, display: 'flex', flexDirection: 'column'}}>
+            <CardContent sx={{flexGrow: 1, p: 4}}>
+                <Typography variant="h5" fontWeight='bold' gutterBottom>{rig.title}</Typography>
+                <Typography variant="body2" fontWeight="semibold" color="text.secondary" paddingBottom="10px">{rig.description}</Typography>
+                <CardMedia component="img" height="300" image={rig.imageUrl} alt={rig.title}/>
             </CardContent>
         </Card>
     );
