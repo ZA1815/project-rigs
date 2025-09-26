f570d4de451b531bc2713ac0b86fb93cad3deaa7
ZA1815 <zaahme18@gmail.com>
2025-09-26 17:01:23 -0400

    chore: update uri to render instead of localhost

--- frontend/src/lib/ApolloClient.js
+++ frontend/src/lib/ApolloClient.js
@@ -2,7 +2,7 @@ import {ApolloClient, InMemoryCache, HttpLink, ApolloLink} from '@apollo/client'
 import {SetContextLink} from '@apollo/client/link/context';
 
 const httpLink = new HttpLink({
-    uri: 'http://localhost:8000/graphql'
+    uri: 'https://project-rigs-backend.onrender.com'
 });
 
 const authLink = new SetContextLink((request, previousContext) => {
